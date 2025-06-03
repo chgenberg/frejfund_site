96eb305e00b63676fe1f157b1136c05c5497657e
Christopher Genberg <christopher@1753skincare.com>
2025-06-03 22:17:19 +0200

    Fix: Move 'use client' to the top of app/page.tsx to resolve Next.js build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+"use client";
 export const dynamic = 'force-dynamic';
 
-"use client";
 import Image from "next/image";
 import { useState } from "react";
 import BusinessPlanWizard from "./components/BusinessPlanWizard";
